d00999445855d0156806e777e9856c7c74147e9d
Fantomas42 <fantomas42@gmail.com>
2013-06-19 10:45:22 +0200

    Compatibility fix for django 1.5

--- relationships/urls.py
+++ relationships/urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import url
+from django.conf.urls import patterns
+
 
 urlpatterns = patterns('relationships.views',
     url(r'^$', 'relationship_redirect', name='relationship_list_base'),
